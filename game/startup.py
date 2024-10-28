5d086849da02c045399d237ee6e52275dd8c92f6
Lucas Henrique Bertanha <l276617@dac.unicamp.br>
2024-10-28 17:12:04 -0300

    Rewrites as functions.

--- game/startup.py
+++ game/startup.py
@@ -1,12 +1,36 @@
 import os.path
 import csv
 
-with open('./game/text/menus.csv', mode='r') as csv_menus:
+LANGUAGE = 'EN.'
+PATH_TO_MENUS = './game/text/menus.csv'
+PATH_TO_SAVE = './save.txt'
+
+# Opens CSV file to get the menu dialogues
+with open(PATH_TO_MENUS, mode='r') as csv_menus:
     menus_dialogue = dict(filter(None, csv.reader(csv_menus)))
 
-PATH_TO_SAVE = './save.txt'
-save_exists = os.path.isfile(PATH_TO_SAVE)
+def menus_csv_handle():
+    menus_csv_exists = os.path.isfile(PATH_TO_MENUS)
 
-if save_exists:
-    save_file = open(PATH_TO_SAVE, mode='w')
-    print(menus_dialogue["EN.file_not_found.error"])
+    if not menus_csv_exists:
+        if LANGUAGE == 'EN.':
+            print("FATAL ERROR :: 'MENUS.CSV' FILE NOT FOUND :: PLEASE DOWNLOAD THE GAME AGAIN")
+        elif LANGUAGE == 'BR.':
+            print("ERRO FATAL :: ARQUIVO 'MENUS.CSV' NÃO ENCONTRADO :: POR FAVOR INSTALE O JOGO NOVAMENTE")
+        
+        quit()
+
+def save_file_handle():
+    save_exists = os.path.isfile(PATH_TO_SAVE)
+
+
+    if save_exists:
+        save_file = open(PATH_TO_SAVE, mode='w')
+
+    else:
+        print(menus_dialogue[LANGUAGE + "file_not_found.error"])
+
+
+
+    pass
+
